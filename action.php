440b2a10f4f6986070869a3029815f120cdc149d
E1Ghrib <elghribabdeloumne@gmail.com>
2022-05-07 19:28:28 +0000

    e

---
+++ action.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Fist Name</td>
+            <td><?= $_POST['fname']?></td>
+        </tr>
+        <tr>
+            <td>Last name</td>
+            <td><?= $_POST['lname']?></td>
+        </tr>
+        <!-- #it was one function  -->
+    </table>
+</body>
+</html>
